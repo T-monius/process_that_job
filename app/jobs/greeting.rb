bca3773fd6e64e9d35407da4877ee2bd27624ef7
mo mac <trrl.mo@hotmail.com>
2020-10-16 15:19:59 -0600

    Rename file "say_hi.rb" to "greeting.rb" to match class name

---
+++ app/jobs/greeting.rb
@@ -0,0 +1,13 @@
+class Greeting < ApplicationJob
+  class BadJobError < ::StandardError; end
+  # attach block for morgue queue?
+  retry_on(BadJobError, wait: 5.seconds, attempts: 3)
+
+  def perform(salutation)
+    succeed = (0..10).to_a.sample > 1
+
+    sleep(3)
+    raise BadJobError unless succeed
+    puts "\n#{salutation}"
+  end
+end
