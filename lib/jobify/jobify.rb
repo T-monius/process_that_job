6a74fe12812fb9de9f985f4e751cf2aa3459daae
mo mac <trrl.mo@hotmail.com>
2020-10-19 11:13:20 -0600

    Convert Jobify class to module; extract a Starter class

--- lib/jobify/jobify.rb
+++ lib/jobify/jobify.rb
@@ -1,36 +1,71 @@
 require_relative 'jobify/active_job_adapter.rb'
 
-class Jobify
-  def initialize; end
+module Jobify
+  class Starter
+    def start
+      self.running = true
+      puts "Starting jobify"
+      puts '---------------------'
 
-  def start
-    self.running = true
-    puts "Starting jobify"
-    puts '---------------------'
+      sleep(1)
+      while running
+        # check queue for jobs and process them
+        puts 'processing those jobs'
+        sleep(5)
+      end
+    rescue SystemExit, Interrupt
+      puts "\nShutting down"
+      sleep(1)
+      shutdown
 
-    sleep(1)
-    while running
-      # check queue for jobs and process them
-      puts 'processing those jobs'
-      sleep(5)
+      exit
     end
-  rescue SystemExit, Interrupt
-    puts "\nShutting down"
-    sleep(1)
-    shutdown
 
-    exit
+    def shutdown
+      self.running = false
+    end
+
+    protected
+
+    attr_accessor :running
   end
-
-  def shutdown
-    self.running = false
-  end
-
-  protected
-
-  attr_accessor :running
 end
 
-job_processor = Jobify.new
+job_processor = Jobify::Starter.new
 
 job_processor.start
+
+# class Jobify
+#   def initialize; end
+
+#   def start
+#     self.running = true
+#     puts "Starting jobify"
+#     puts '---------------------'
+
+#     sleep(1)
+#     while running
+#       # check queue for jobs and process them
+#       puts 'processing those jobs'
+#       sleep(5)
+#     end
+#   rescue SystemExit, Interrupt
+#     puts "\nShutting down"
+#     sleep(1)
+#     shutdown
+
+#     exit
+#   end
+
+#   def shutdown
+#     self.running = false
+#   end
+
+#   protected
+
+#   attr_accessor :running
+# end
+
+# job_processor = Jobify.new
+
+# job_processor.start
